4e21ca08a7b18b050ca52acdfa5714f0576a2cff
16130174 <Bubblegum.no@yandex.ru>
2018-04-01 23:46:34 +0500

    2
    Второй студент

--- qw_.java
+++ qw_.java
@@ -106,3 +106,41 @@ srok.setFont(new Font("Tahoma", Font.PLAIN, 14));
 srok.setBounds(189, 167, 173, 20);
 frame.getContentPane().add(srok);
 srok.setColumns(10);
+srok.addKeyListener(new KeyAdapter() {//добавление слушател€ на поле дл€ ввода
+public void keyTyped(KeyEvent e) {
+char c = e.getKeyChar();
+if ( ((c < '0') || (c > '9'))) {
+e.consume(); // запрет ввода символов
+}
+}
+});
+JLabel lblNewLabel_1 = new JLabel("\u0421\u0443\u043C\u043C\u0430 \u0432\u043A\u043B\u0430\u0434\u0430, \u0440\u0443\u0431"); //объ€вл€ютс€ текстовые строки
+lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+lblNewLabel_1.setBounds(24, 100, 149, 37);
+frame.getContentPane().add(lblNewLabel_1); //добавление метки на панель
+JLabel lblNewLabel_2 = new JLabel("\u041F\u0440\u043E\u0446\u0435\u043D\u0442\u043D\u0430\u044F \u0441\u0442\u0430\u0432\u043A\u0430,%"); // объ€вл€ютс€ текстовые строки
+lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+lblNewLabel_2.setBounds(24, 221, 158, 37);
+frame.getContentPane().add(lblNewLabel_2);
+//объ€вл€ютс€ текстовые строки
+lblNewLabel_3 = new JLabel("\u0421\u0440\u043E\u043A \u0432\u043B\u043E\u0436\u0435\u043D\u0438\u044F, \u043C\u0435\u0441");
+lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+
+lblNewLabel_3.setBounds(24, 159, 149, 36);
+frame.getContentPane().add(lblNewLabel_3);
+chek = new JCheckBox("\u0431\u0435\u0437 \u0435\u0436\u0435\u043C\u0435\u0441\u044F\u0447\u043D\u043E\u0439 \u043A\u0430\u043F\u0438\u0442\u0430\u043B\u0438\u0437\u0430\u0446\u0438\u0438"); //объ€вл€етс€ флажок
+chek.setSelected(true); //флажок включен
+chek.setBounds(313, 286, 266, 25);
+frame.getContentPane().add(chek);
+//объ€вл€ютс€ текстовые строки
+JLabel lblNewLabel_4 = new JLabel("\u0418\u0442\u043E\u0433\u043E\u0432\u0430\u044F \u0441\u0443\u043C\u043C\u0430 \u043D\u0430 \u043A\u043E\u043D\u0435\u0446 \u0441\u0440\u043E\u043A\u0430");  
+lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
+lblNewLabel_4.setBounds(24, 436, 229, 30);
+frame.getContentPane().add(lblNewLabel_4);
+JLabel lblNewLabel_6 = new JLabel("\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0434\u0430\u043D\u043D\u044B\u0435 \u043E \u0432\u043A\u043B\u0430\u0434\u0447\u0438\u043A\u0435:");
+lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 14));
+lblNewLabel_6.setBounds(65, 40, 234, 20);
+frame.getContentPane().add(lblNewLabel_6);
+ }
+}
+
